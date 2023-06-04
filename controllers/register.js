69d56683273a76577b902010c96d52fee1d0383c
davidforespi <90919731+davidforespi@users.noreply.github.com>
2023-06-04 00:02:50 -0500

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -33,8 +33,7 @@ const sendConfirmationEmail = (correo) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'godrestaurant13@gmail.com',
-            pass: 'deekjduvulgzjink'
+
         },
     });
 
