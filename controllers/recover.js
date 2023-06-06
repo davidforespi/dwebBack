6143d14748786994a1358038dd521a09c4337b08
davidforespi <90919731+davidforespi@users.noreply.github.com>
2023-06-06 14:38:15 -0500

    Update recover.js

--- controllers/recover.js
+++ controllers/recover.js
@@ -23,8 +23,7 @@ const sendConfirmationEmail = (correo) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'godrestaurant13@gmail.com',
-            pass: 'deekjduvulgzjink',
+
         },
     });
 
